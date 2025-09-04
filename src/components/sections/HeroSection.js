33a91a7dc6236cf3b7c3d9c12b791d165e7f6e75
samfred <samfred474@gmail.com>
2025-09-04 03:14:30 +0100

    new commit

--- src/components/sections/HeroSection.js
+++ src/components/sections/HeroSection.js
@@ -5,7 +5,7 @@ import { itemVariants } from "@/lib/motion";
 
 export default function HeroSection() {
   return (
-    <motion.section id="hero" className="relative min-h-[70vh]  text-center mt-24 pb-20 md:pt-20 md:pb-24 flex flex-col justify-center items-center " variants={itemVariants}>
+    <motion.section id="hero" className="relative min-h-[70vh]  text-center  pb-20 md:pt-14 md:pb-24 flex flex-col justify-center items-center " variants={itemVariants}>
       <div className="absolute inset-0 z-0" />
       <HeroSlider />
     </motion.section>
